92960b051d0f041f552e575c623fa33fc7f29504
lhj0621 <lhj0621@gmail.com>
2020-04-20 21:41:36 +0900

    getlist 추가

--- Model/country.js
+++ Model/country.js
@@ -1,6 +1,16 @@
 const fs = require('fs');
 
 class country {
+    constructor() {
+        const data = fs.readFileSync('./model/data.json');
+        this.data = JSON.parse(data)
+    }
+
+    getcontryList(){
+        if (this.data){
+            return this.data;
+        }
+    }
 
 }
 module.exports = new country();
--- Router/countryRouter.js
+++ Router/countryRouter.js
@@ -2,5 +2,12 @@ const express = require('express');
 const router = express.Router();
 const country = require('../model/country');
 
+router.get('/country', showcountryList);
+
 
 module.exports = router;
+
+function showcountryList(req,res) {
+    const countryList = country.getcontryList();
+    res.send(countryList);
+}
