b12b3bcac3b214f0ed3f246769047295fe3362ec
lhj0621 <lhj0621@gmail.com>
2020-04-20 23:36:22 +0900

    getdetail 제작중

--- Model/country.js
+++ Model/country.js
@@ -3,13 +3,24 @@ const fs = require('fs');
 class country {
     constructor() {
         const data = fs.readFileSync('./model/data.json');
-        this.data = JSON.parse(data)
+        this.data = JSON.parse(data);
     }
 
     getcontryList(){
         if (this.data){
             return this.data;
         }
+    }
+
+    getcontrydetal(id){
+        return new Promise((resolve,reject)=>{
+            for(var object of this.data){
+                console.log(object);
+                resolve(object);
+                return;
+            }
+            reject({msg:id+' not found',cod:404});
+       });
     }
 
 }
--- Router/countryRouter.js
+++ Router/countryRouter.js
@@ -2,12 +2,21 @@ const express = require('express');
 const router = express.Router();
 const country = require('../model/country');
 
-router.get('/country', showcountryList);
+
+router.get('/country', (req, res) => {
+
+    const countryList = country.getcontryList();
+    const data = countryList;
+    const result = { count: data.length, data: data };
+    res.send(result);
+
+});
+
+router.get('/country/:id', (req, res) => {
+    const id = req.params.id;
+    const data = country.getcontrydetal(id);
+    res.send(data);
+});
 
 
 module.exports = router;
-
-function showcountryList(req,res) {
-    const countryList = country.getcontryList();
-    res.send(countryList);
-}
