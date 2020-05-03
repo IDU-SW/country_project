9ee7d8bb1f7b6632934924ca03ef3a2bdcd3016f
lhj0621 <lhj0621@gmail.com>
2020-05-03 15:53:18 +0900

    삭제전 confirm추

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,10 @@
 function deleteajax(id) {
-    $.ajax({
-        url: '/country/' + id,
-        method: "DELETE",
-    }).done(function (response) {
-        location.href = "/";
-    });
+    if (confirm("삭제 하시겠습니까?")) {
+        $.ajax({
+            url: '/country/' + id,
+            method: "DELETE",
+        }).done(function (response) {
+            location.href = "/";
+        });
+    }
 }
