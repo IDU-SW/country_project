a4870844d015d0dec28c4d97d1cfeb50039a5f46
lhj0621 <lhj0621@gmail.com>
2020-05-19 22:26:12 +0900

    추가 / 디테일 / 리스트 추가

--- model/country.js
+++ model/country.js
@@ -1,54 +1,63 @@
 const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID;
 
 class country {
     constructor() {
-        const data = fs.readFileSync('./model/data.json');
-        this.data = JSON.parse(data);
+        const url = 'mongodb://localhost:27017/example';
+        MongoClient.connect(url, { useUnifiedTopology: true }, async (err, client) => {
+            if (err) {
+                console.error('MongoDB 연결 실패', err);
+                return;
+            }
+
+            this.db = client.db();
+        });
     }
 
-    getcontryList() {
-        if (this.data) {
-            return this.data;
+    async getcontryList() {
+        const country = this.db.collection('country');
+        try {
+            let docs = await country.find().toArray();
+            return docs;
+        }
+        catch (error) {
+            console.log('getcontryList Error', error);
         }
     }
 
-    getcontrydetail(id) {
-        return new Promise((resolve, reject) => {
-            for (var object of this.data) {
-                if (object.id == id) {
-                    resolve(object);
-                    return;
-                }
-            }
-            reject({ msg: id + ' not found', code: 404 });
-        });
+    async getcontrydetail(id) {
+        const country = this.db.collection('country');
+        try {
+            const docId = new ObjectID(id);
+            let docs = await country.findOne({ _id: docId });
+            return docs;
+        }
+        catch (error) {
+            console.log('getcontrydetail Error', error);
+        }
     }
 
-    addcontry(data) {
-        return new Promise((resolve, reject) => {
-            const add_idx = this.data[this.data.length - 1].id + 1;
-            const newcontry = {
-                id:add_idx,
-                country:data.country,
-                capital:data.capital,
-                area:data.area,
-                language:data.language,
-                currency:data.currency
-            }
-            this.data.push(newcontry);
-            resolve(newcontry);
-        });
+    async addcontry(data) {
+        const country = this.db.collection('country');
+        try {
+            let result = await country.insertOne(data);
+            return await this.getcontrydetail(result.insertedId.toString());
+        }
+        catch (error) {
+            console.log('addcontry Error', error);
+        }
     }
 
-    updatecontry(data){
+    updatecontry(data) {
         return new Promise((resolve, reject) => {
             for (var object of this.data) {
                 if (object.id == data.id) {
-                    object.country=data.country;
-                    object.capital=data.capital;
-                    object.area=data.area;
-                    object.language=data.language;
-                    object.currency=data.currency;
+                    object.country = data.country;
+                    object.capital = data.capital;
+                    object.area = data.area;
+                    object.language = data.language;
+                    object.currency = data.currency;
                     resolve(object);
                     return;
                 }
@@ -56,12 +65,12 @@ class country {
             reject({ msg: id + ' not update', code: 404 });
         });
     }
-    deltecountry(id){
+    deltecountry(id) {
         return new Promise((resolve, reject) => {
             for (var object of this.data) {
                 if (object.id == id) {
                     const search = this.data.indexOf(object);
-                    this.data.splice(search,1);
+                    this.data.splice(search, 1);
                     resolve(object.id);
                     return;
                 }
