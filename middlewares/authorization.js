0d77b1945c3a1002c4c861032051712ae5f5d409
lhj0621 <lhj0621@gmail.com>
2020-06-30 01:04:54 +0900

    토큰 검증 미들웨어 추가

---
+++ middlewares/authorization.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const config = require(__dirname + '/../config/jwt_key.json')
+const YOUR_SECRET_KEY = config.secretKey;
+
+const verifyToken = (req, res, next) => {
+    try {
+        const clientToken = req.cookies.user;
+        const decoded = jwt.verify(clientToken, YOUR_SECRET_KEY);
+        if (decoded) {
+            res.locals.userId = decoded.user_id;
+            next();
+        } else {
+            res.status(401).json({ error: 'unauthorized' });
+        }
+    } catch (err) {
+        res.status(401).json({ error: 'token expired' });
+    }
+};
+exports.verifyToken = verifyToken;
